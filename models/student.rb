f311a7e8eb802d6e3d24a42e42bc26715a1ca60c
Murray McQuarrie <murray.mcquarrie@gmail.com>
2017-02-01 11:44:44 +0000

    added save function, student class and sqlrunner. Have a nice day

---
+++ models/student.rb
@@ -0,0 +1,19 @@
+require_relative('../db/sql_runner')
+
+class Student 
+
+  def initialize (options)
+    @id = nil || options['id'].to_i
+    @first_name = options['first_name']
+    @last_name = options['last_name']
+    @house = options['house']
+    @age = options['age'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO students(first_name,last_name,house,age) VALUES ('#{@first_name}', '#{last_name}', '#{house}', #{age}') RETURNING *"
+    student_data = SqlRunner.run(sql)
+    @id = student_data.first()['id'].to_i 
+  end
+
+end
